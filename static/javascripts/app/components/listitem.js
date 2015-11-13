e4690e89061bd782b5f0d15552945998f8a8f62c
Samuel Ng <sam@infinitely.io>
2015-11-13 17:25:11 -0500

    Add list item view class

---
+++ static/javascripts/app/components/listitem.js
@@ -0,0 +1,30 @@
+
+var ListItem = function ListItem(options) {
+    'use strict';
+    return App.Common.View.call(this, options);
+};
+
+ListItem.prototype = Object.create(App.Common.View.prototype);
+ListItem.prototype.constructor = ListItem;
+
+ListItem.prototype.controller = function(opts) {
+    if (!this.get('initialized')) {
+        // Set item status as initialized
+        this.set('initialized', true);
+        // Patch component object
+        this.component(opts);
+        // Link stylesheet
+        this.stylesheet(['components/listitem.css']);
+        // Create and modifiy element
+    }
+};
+
+ListItem.prototype.view = function() {
+    return m('li', [
+
+    ]);
+};
+
+App.Components.ListItem = ListItem;
+
+
