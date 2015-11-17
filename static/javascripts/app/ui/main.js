95b63cab059f4d0e2302207204b93fd1346d6bf2
Samuel Ng <sam@infinitely.io>
2015-11-16 22:09:49 -0500

    update Main UI class, retrieve shots from main process

--- static/javascripts/app/ui/main.js
+++ static/javascripts/app/ui/main.js
@@ -1,28 +1,31 @@
 
 var MainUI = function MainUI(options) {
     'use strict';
-    App.Common.View.call(this, options);
-    // Set debugging mode
-    this.set('debugging', true);
-    return this;
+    return App.Common.View.call(this, options);
 };
 
 MainUI.prototype = Object.create(App.Common.View.prototype);
 MainUI.prototype.constructor = MainUI;
 
 MainUI.prototype.controller = function() {
-    // Link stylesheet
-    this.stylesheet(['core/application.css', 'ui/main.css']);
-    // Create and modifiy element
-    this.set('ui.component.sidebar', new App.Components.Sidebar({
-        'name': 'sidebar'
-    }));
-    this.set('ui.component.statusbar', new App.Components.Statusbar({
-        'name': 'statusbar'
-    }));
-    this.set('ui.component.toolbar', new App.Components.Toolbar({
-        'name': 'toolbar'
-    }));
+    var ui = this.instance();
+    if (!this.get('initialized')) {
+        // Set item status as initialized
+        this.set('initialized', true);
+        // Link stylesheet
+        this.stylesheet(['core/application.css', 'ui/main.css']);
+        // Create and modifiy element
+        this.set('ui.component.sidebar', new App.Components.Sidebar({
+            'name': 'sidebar'
+        }));
+        this.set('ui.component.statusbar', new App.Components.Statusbar({
+            'name': 'statusbar'
+        }));
+        this.set('ui.component.toolbar', new App.Components.Toolbar({
+            'name': 'toolbar'
+        }));
+        this.on('shots', ui._retrieveShots.bind(ui));
+    }
 };
 
 MainUI.prototype.view = function() {
@@ -31,6 +34,15 @@ MainUI.prototype.view = function() {
         m.component(this.get('ui.component.statusbar')),
         m.component(this.get('ui.component.toolbar'))
     ];
+};
+
+MainUI.prototype.unload = function() {
+    var ui = this.instance();
+    this.off('shots', ui._retrieveShots.bind(ui));
+};
+
+MainUI.prototype._retrieveShots = function(event, err, res) {
+    console.log(err, res);
 };
 
 App.UI.MainUI = MainUI;
